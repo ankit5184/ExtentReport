a78427c1c74c01a2285d4c454237f25f2e19b183
ankitkumar5184 <96471233+ankitkumar5184@users.noreply.github.com>
2022-10-18 16:57:54 +0530

    Update Jenkins.bat

--- Jenkins.bat
+++ Jenkins.bat
@@ -1,1 +1,29 @@
-java -Dhudson.model.DirectoryBrowserSupport.CSP="" -jar "C:\Users\AnkitKumar\Downloads\jenkins.war" --httpPort=8082
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: "main", url:'https://github.com/ankitkumar5184/JavaFrameworkExtentReport.git
+            }
+        }
+        stage('Test') {
+            steps{
+                xml 'testng.xml'
+            }
+        }
+    }
+   post {
+        	always {
+          echo 'test executed'
+	  	}
+	   success {  
+             echo 'Build successfully executed'
+         }  
+       failure {
+         mail to: 'ankit.kumar@infostride.com',
+             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             body: "Build number ${env.BUILD_NUMBER} has been failed. Track the issues by visiting the url  ${env.BUILD_URL}"
+	    }
+    }
+}
+	
